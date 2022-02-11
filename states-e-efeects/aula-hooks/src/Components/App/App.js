12321dd956938e7028cd278c6d64889e792a1699
JFernando <almo_fernando@hotmail.com>
2022-02-11 00:56:06 -0300

    Aula de Hooks

---
+++ states-e-efeects/aula-hooks/src/Components/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Counter from '../Counter/Counter';
+import SmartCounter from '../Smartcounter/Smartcounter';
+import Ifoodcounter from '../Ifoodcounter/ifoodcounter';
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Ifoodcounter/>
+    </>
+    
+  );
+}
+
+export default App;
